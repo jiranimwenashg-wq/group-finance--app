7c7a49696f3b97476d5c2dc00a20bb01e6d2fb61
Nduat Njorogr <jiranimwenashg@gmail.com>
2025-11-15 18:54:58 +0000

    Try fixing this error: `Runtime Error: Error: (0 , firebase_auth__WEBPAC

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,38 +5,46 @@ import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
 
-// IMPORTANT: DO NOT MODIFY THIS FUNCTION
-export function initializeFirebase() {
-  if (!getApps().length) {
-    // Important! initializeApp() is called without any arguments because Firebase App Hosting
-    // integrates with the initializeApp() function to provide the environment variables needed to
-    // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
-    // without arguments.
-    let app;
-    try {
-      // Attempt to initialize via Firebase App Hosting environment variables
-      app = initializeApp();
-    } catch (e) {
-      // Only warn in production because it's normal to use the firebaseConfig to initialize
-      // during development
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      app = initializeApp(firebaseConfig);
+let app: FirebaseApp;
+let auth: Auth;
+let firestore: Firestore;
+
+if (!getApps().length) {
+  try {
+    app = initializeApp();
+  } catch (e) {
+    if (process.env.NODE_ENV === 'production') {
+      console.warn(
+        'Automatic initialization failed. Falling back to firebase config object.',
+        e
+      );
     }
-
-    return getSdks(app);
+    app = initializeApp(firebaseConfig);
   }
-
-  // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
+} else {
+  app = getApp();
 }
 
-export function getSdks(app: FirebaseApp): { firebaseApp: FirebaseApp, auth: Auth, firestore: Firestore } {
+auth = getAuth(app);
+firestore = getFirestore(app);
+
+export function initializeFirebase() {
+  return {
+    firebaseApp: app,
+    auth,
+    firestore,
+  };
+}
+
+export function getSdks(app: FirebaseApp): {
+  firebaseApp: FirebaseApp;
+  auth: Auth;
+  firestore: Firestore;
+} {
   return {
     firebaseApp: app,
     auth: getAuth(app),
-    firestore: getFirestore(app)
+    firestore: getFirestore(app),
   };
 }
 
