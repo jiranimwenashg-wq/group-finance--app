89937c1ecf780cf30614f7611f957fa39006d23a
Nduat Njorogr <jiranimwenashg@gmail.com>
2025-11-15 19:08:36 +0000

    import { initializeApp, getApps, getApp } from "firebase/app"; import {

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -5,17 +5,27 @@ import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
 
+let firebaseApp: FirebaseApp;
+let auth: Auth;
+let firestore: Firestore;
+
+// Initialize Firebase
+if (getApps().length === 0) {
+  firebaseApp = initializeApp(firebaseConfig);
+} else {
+  firebaseApp = getApp();
+}
+
+auth = getAuth(firebaseApp);
+firestore = getFirestore(firebaseApp);
+
 export function initializeFirebase(): {
   firebaseApp: FirebaseApp;
   auth: Auth;
   firestore: Firestore;
 } {
-  const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-  const auth = getAuth(app);
-  const firestore = getFirestore(app);
-
   return {
-    firebaseApp: app,
+    firebaseApp,
     auth,
     firestore,
   };
