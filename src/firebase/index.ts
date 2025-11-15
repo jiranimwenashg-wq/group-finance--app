9da0805b3c31107ba8ecf47a24c01e37c20a40cd
Nduat Njorogr <jiranimwenashg@gmail.com>
2025-11-15 18:52:43 +0000

    Try fixing this error: `Runtime Error: Error: (0 , firebase_auth__WEBPAC

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,42 +1,42 @@
 'use client';
 
 import { firebaseConfig } from '@/firebase/config';
-import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore'
+import * as firebaseApp from 'firebase/app';
+import * as firebaseAuth from 'firebase/auth';
+import * as firestore from 'firebase/firestore';
 
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
-  if (!getApps().length) {
+  if (!firebaseApp.getApps().length) {
     // Important! initializeApp() is called without any arguments because Firebase App Hosting
     // integrates with the initializeApp() function to provide the environment variables needed to
     // populate the FirebaseOptions in production. It is critical that we attempt to call initializeApp()
     // without arguments.
-    let firebaseApp;
+    let app;
     try {
       // Attempt to initialize via Firebase App Hosting environment variables
-      firebaseApp = initializeApp();
+      app = firebaseApp.initializeApp();
     } catch (e) {
       // Only warn in production because it's normal to use the firebaseConfig to initialize
       // during development
       if (process.env.NODE_ENV === "production") {
         console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
       }
-      firebaseApp = initializeApp(firebaseConfig);
+      app = firebaseApp.initializeApp(firebaseConfig);
     }
 
-    return getSdks(firebaseApp);
+    return getSdks(app);
   }
 
   // If already initialized, return the SDKs with the already initialized App
-  return getSdks(getApp());
+  return getSdks(firebaseApp.getApp());
 }
 
-export function getSdks(firebaseApp: FirebaseApp) {
+export function getSdks(app: firebaseApp.FirebaseApp) {
   return {
-    firebaseApp,
-    auth: getAuth(firebaseApp),
-    firestore: getFirestore(firebaseApp)
+    firebaseApp: app,
+    auth: firebaseAuth.getAuth(app),
+    firestore: firestore.getFirestore(app)
   };
 }
 
