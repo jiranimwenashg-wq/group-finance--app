a1b8061cbd17e31758991c8314a16554d70f8db5
Nduat Njorogr <jiranimwenashg@gmail.com>
2025-11-17 04:09:31 +0000

    ADD THIS CSP HEADER:====// next.config.js module.exports = {   async hea

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,28 @@ const pwaConfig = withPWA({
 });
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com https://www.googleadservices.com https://www.gstatic.com https://apis.google.com blob:;
+              worker-src 'self' blob: data:;
+              img-src 'self' blob: data: https://*;
+              connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://www.googleadservices.com https://*.google.com https://*.googleapis.com https://*.firebaseio.com wss://*.firebaseio.com https://*.gstatic.com blob:;
+              frame-src https://www.youtube.com https://www.youtube-nocookie.com https://www.googletagmanager.com https://*.firebaseapp.com;
+              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+              font-src 'self' https://fonts.gstatic.com data:;
+            `.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
