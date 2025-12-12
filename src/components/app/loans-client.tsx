3efd82d3d34f20bea3f624188d8d2c67a62d3414
Nduat Njorogr <jiranimwenashg@gmail.com>
2025-12-12 03:15:09 +0000

    Try fixing this error: `Unhandled Runtime Error: RangeError: Invalid tim

--- src/components/app/loans-client.tsx
+++ src/components/app/loans-client.tsx
@@ -543,7 +543,7 @@ export default function LoansClient() {
                                                 !field.value && "text-muted-foreground"
                                             )}
                                             >
-                                            {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+                                            {field.value instanceof Date && !isNaN(field.value.getTime()) ? format(field.value, "PPP") : <span>Pick a date</span>}
                                             <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
                                             </Button>
                                         </FormControl>
